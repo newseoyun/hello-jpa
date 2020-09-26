aa4cdd184abfe381517706da5f384d2778449cb6
seoyun <newseoyun@gmail.com>
2020-09-26 17:15:26 +0900

    연관관계 매핑

---
+++ src/main/java/jpashop/jpaShopMain.java
@@ -0,0 +1,38 @@
+package jpashop;
+
+import jpashop.domain.Order;
+import jpashop.domain.OrderItem;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class jpaShopMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+
+            Order order = new Order();
+            order.addOrderItem(new OrderItem());
+
+
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+}
